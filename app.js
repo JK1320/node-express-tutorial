5b27101ab0685c02fe4c06eeed3d17cb2bf91fb1
JK1320 <kaurjus1@gmail.com>
2022-03-17 13:26:15 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+// setInterval(()=> {
+//     console.log("Hello World");
+// }, 1000)
+
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
