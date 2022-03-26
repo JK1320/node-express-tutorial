99a9220a37267c3be24efe976fccb37fd56fc72c
JK1320 <kaurjus1@gmail.com>
2022-03-26 12:24:35 +0000

    added comments

--- 2-express-tutorial/final/06-basic-json.js
+++ 2-express-tutorial/final/06-basic-json.js
@@ -8,3 +8,13 @@ app.get('/', (req, res) => {
 app.listen(5000, () => {
   console.log('Server is listening on port 5000....')
 })
+
+
+// express has 2 following options: to set up APIs or template with server side rendering (SSR)
+// API: setting up http interface to interact with data
+// data is sent using json (javascript object notation)
+// send response with res.json() method
+
+
+//SSR: send entire template ourselves; html, css, javascript
+// send this with res.render() method
