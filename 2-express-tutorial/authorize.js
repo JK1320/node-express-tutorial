8d8f2dbe46482ad8483f075d469a9b249d089419
JK1320 <kaurjus1@gmail.com>
2022-03-30 10:32:02 +0100

    added authorize middleware

---
+++ 2-express-tutorial/authorize.js
@@ -0,0 +1,13 @@
+const authorize = (req, res, next) => {
+  //query string
+  const { user } = req.query;
+
+  if (user === 'john') {
+    req.user = { name: 'john', id: 3 };
+    next();
+  } else {
+      res.status(401).send('Unauthorized');
+  }
+};
+
+module.exports = authorize;
