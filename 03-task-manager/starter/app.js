190a793a570ec71f1d5049d948a0a5208a641855
JK1320 <kaurjus1@gmail.com>
2022-04-07 23:16:13 +0100

    databse connected

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 // middleware - if we don't use this we won't have data in req.body as req sends data in json
 app.use(express.json());
@@ -21,5 +23,13 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`Server is listening on port ${port}...`)
-)
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (err) {
+      console.log(err);
+  }
+};
+
+start();
