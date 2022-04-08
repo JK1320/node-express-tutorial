feba3f988df6f9e97034d7007f906d715be78622
JK1320 <kaurjus1@gmail.com>
2022-04-08 23:46:54 +0100

    database schema

--- 03-task-manager/starter/models/task.js
+++ 03-task-manager/starter/models/task.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose'); // import mongoose
 
 const TaskSchema = new mongoose.Schema({
-
+name:String,completed:Boolean
 });
+
+
+module.exports = mongoose.model('Task', TaskSchema);
