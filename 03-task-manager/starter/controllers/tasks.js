f6eb04ec988698a0fbc7b1d6961c6e23c8b3e9a9
JK1320 <kaurjus1@gmail.com>
2022-04-09 00:46:52 +0100

    added get all tasks function

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,27 +1,36 @@
 const Task = require('../models/task');
 
-const getAllTasks = (req,res) => {
-res.send('All items from the file');
+//get all tasks
+const getAllTasks = async (req,res) => {
+try {
+  const tasks = await Task.find({});
+  res.status(200).json({ tasks });
+} catch (error) {
+  res.status(500).json({ msg: error });
+}
 };
 
+//create new task
 const createTask = async (req, res) => {
   try {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
   } catch (error) {
     res.status(500).json({msg: error});
-  }
-  
+  }  
 };
 
+//get one task
 const getTask = (req, res) => {
   res.json({id: req.params.id});
 };
 
+// update a task
 const updateTask = (req, res) => {
   res.send("Update Task");
 };
 
+//delete a task
 const deleteTask = (req, res) => {
   res.send("Delete Task");
 };
