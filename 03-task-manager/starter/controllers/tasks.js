0d291c2e351ffe9662e49221dee973085f49df0d
JK1320 <kaurjus1@gmail.com>
2022-04-10 04:49:42 +0100

    respose options comment added

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -5,7 +5,11 @@ const Task = require('../models/task');
 const getAllTasks = async (req,res) => {
 try {
   const tasks = await Task.find({});
-  res.status(200).json({ tasks });
+  // response options
+   res.status(200).json({ tasks });
+  //res.status(200).json({ tasks, amount: tasks.length });
+  // res.status(200).json({ success: true, data: {tasks, nbHits: tasks.length} });
+// res.status(200).json({ status: 'success', data: { tasks, nbHits: tasks.length } });
 } catch (error) {
   res.status(500).json({ msg: error });
 }
