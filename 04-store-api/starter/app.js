85651891028f9e5c3e4375c93f69fbe30dfa9a01
JK1320 <kaurjus1@gmail.com>
2022-05-03 23:48:24 +0100

    added get routes

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,2 +1,42 @@
 require('dotenv').config();
+// async errors
+
+const express = require('express');
+const app = express();
+
+
+
+const notFoundMiddleware = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+//middleware
+// not using in this project - only for practice so I remember to use it in the other projects
+app.use(express.json());
+
+// routes
+
+app.get('/', (req, res) => {
+    res.send('<h1>STORE API</h1><a href="/api/v1/products">Products route</a>')
+});
+
+//products routes
+
+
+
+app.use(notFoundMiddleware);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+    try {
+       // connectDB
+       app.listen(port, console.log(`Server is listening on port ${port} ...`)) 
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
+
 
