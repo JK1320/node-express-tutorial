37071f10f3c2b358df34507a4dd19dc408996125
JK1320 <kaurjus1@gmail.com>
2022-06-13 01:20:44 +0100

    import products route to app.js

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const app = express();
 
 const connectDB = require("./db/connect");  // returns promise
+const productsRouter = require("./routes/products");
 
 const notFoundMiddleware = require('./middleware/not-found');
 const errorMiddleware = require('./middleware/error-handler');
@@ -18,6 +19,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
     res.send('<h1>STORE API</h1><a href="/api/v1/products">Products route</a>')
 });
+
+app.use('/api/v1/products', productsRouter)
 
 //products routes
 
